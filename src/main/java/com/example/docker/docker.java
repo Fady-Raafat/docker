af0aed6f8db579983b07b3cb2a8c55f84f65b3ed
fady raafat <fady.raafat@media-sci.com>
2021-04-24 10:15:09 +0200

    hello

---
+++ src/main/java/com/example/docker/docker.java
@@ -0,0 +1,14 @@
+package com.example.docker;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/rest/")
+public class docker {
+    @GetMapping
+    public String hello(){
+        return "hello";
+    }
+}
